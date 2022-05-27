18e83dce0dee44eaac93eca18695d95ddb7a335d
Nikita Tikhonov <tn@smarthead.ru>
2022-05-27 15:03:35 +0300

    SHNSITE-9 Add Stylelint

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,7 @@ const config: GatsbyConfig = {
         {
             resolve: 'gatsby-plugin-manifest',
             options: {
-                'icon': 'src/images/icon.png',
+                'icon': 'src/assets/images/icon.png',
             }
         },
     ]
