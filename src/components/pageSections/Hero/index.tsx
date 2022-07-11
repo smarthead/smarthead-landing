5804fe1ce1309418bec46dd3cf963a6421ff9ea2
Biarslan <bf@smarthead.digital>
2022-07-11 20:03:44 +0300

    Added responsive layout for Hero section

--- src/components/pageSections/Hero/index.tsx
+++ src/components/pageSections/Hero/index.tsx
@@ -1,34 +1,67 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
 import Header from '../../layout/Header';
 import ButtonLink from '../../shared/ButtonLink';
 import * as styles from './index.module.scss';
 
-const Hero: React.FC = () => (
-    <section className={`${styles.hero} container`}>
-        <div className={styles.header}>
-            <Header />
-        </div>
-        <div className={styles.content}>
-            <h1 className={styles.headline}>
-                Разрабатываем
-                <br />
-                <span className={styles.secondLine}>амбициозные</span>
-                <br />
-                цифровые продукты
-            </h1>
-            <div className={styles.block}>
-                <ButtonLink
-                    type="yellow"
-                    text="ХОЧУ С ВАМИ РАБОТАТЬ"
-                    link="#"
-                />
-                <p className={styles.subtext}>
-                    Продуктоориентированная команда,
-                    <br /> а не просто «руки на аутстаффе»
-                </p>
+const Hero: React.FC = () => {
+    const resize = () => {
+        const secondLineElem = document.getElementsByClassName(
+            styles.secondLine
+        )[0] as HTMLElement;
+        const subtextElem = document.getElementsByClassName(
+            styles.subtext
+        )[0] as HTMLElement;
+
+        const width = window.innerWidth;
+        if (width > 992 && width <= 1281) {
+            subtextElem.style.marginRight =
+                0.98 * (secondLineElem.offsetWidth - subtextElem.offsetWidth) +
+                'px';
+        } else {
+            subtextElem.style.marginRight = '0';
+        }
+    };
+
+    useEffect(() => {
+        const fontGilroyBold = new FontFaceObserver('Gilroy-Bold');
+        const fontInterRegular = new FontFaceObserver('Inter-Regular');
+
+        Promise.all([fontGilroyBold.load(), fontInterRegular.load()]).then(
+            () => {
+                resize();
+            }
+        );
+
+        window.addEventListener('resize', resize);
+        return () => window.removeEventListener('resize', resize);
+    }, []);
+
+    return (
+        <section className={`${styles.hero} container`}>
+            <div className={styles.header}>
+                <Header />
             </div>
-        </div>
-    </section>
-);
+            <div className={styles.content}>
+                <h1 className={styles.headline}>
+                    Разрабатываем
+                    <span className={styles.secondLine}>амбициозные</span>
+                    цифровые продукты
+                </h1>
+                <div className={styles.block}>
+                    <ButtonLink
+                        type="yellow"
+                        text="ХОЧУ С ВАМИ РАБОТАТЬ"
+                        link="#"
+                    />
+                    <p className={styles.subtext}>
+                        Продуктоориентированная команда,
+                        <br /> а не просто «руки на аутстаффе»
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
 
 export default Hero;
