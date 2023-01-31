46c79fd883fca792e71529299df677a8bd03bed9
Nikita Negodov <nn@smarthead.ru>
2023-01-31 18:30:09 +0300

    SHNSITE-42 added common FooterContactsContainer component

--- src/components/pageSections/FooterContacts/index.tsx
+++ src/components/pageSections/FooterContacts/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import cn from 'classnames';
-import { links } from '../../shared/links';
 
+import { FooterContactsContainer } from '../../shared/FooterContactsContainer';
+
+import { links } from '../../shared/links';
 import shLogo from '../../../assets/images/SmartHead-Logo.svg';
 import * as styles from './index.module.scss';
 
@@ -49,72 +51,65 @@ const socialMediaLinks = [
 ];
 
 export const FooterContacts: React.FC = () => (
-    <footer className={styles.root}>
-        <div className="container">
-            <div className={styles.content}>
-                <img
-                    src={shLogo}
-                    alt="SmartHead Logo"
-                    className={styles.logo}
-                />
+    <FooterContactsContainer>
+        <div className={styles.content}>
+            <img src={shLogo} alt="SmartHead Logo" className={styles.logo} />
 
-                <div className={styles.contacts}>
-                    <div className={styles.contactsItem}>
-                        <a href="tel:+78432060726" target="_blank">
-                            +7 843 206 07 26
+            <div className={styles.contacts}>
+                <div className={styles.contactsItem}>
+                    <a href="tel:+78432060726" target="_blank">
+                        +7 843 206 07 26
+                    </a>
+                </div>
+                Петербургская, 50, Казань, 420000
+            </div>
+
+            <div className={cn(styles.contacts, styles.links)}>
+                {socialMediaLinks.map(({ name, link, id }, i) => (
+                    <div
+                        key={id}
+                        className={cn(styles.contactsItem, {
+                            [styles.penultimate]: id === 4,
+                            [styles.last]: i === socialMediaLinks.length - 1,
+                        })}
+                    >
+                        <a href={link} target="_blank">
+                            {name}
                         </a>
                     </div>
-                    Петербургская, 50, Казань, 420000
+                ))}
+            </div>
+        </div>
+
+        <hr className={styles.line} />
+
+        <div className={cn(styles.contacts, styles.bottomContainer)}>
+            <div className={styles.bottomContainerFirstBlock}>
+                <div
+                    className={cn(
+                        styles.bottomContainerItem,
+                        styles.bottomContainerFistItem
+                    )}
+                >
+                    <a href={links.privacyPolicy} target="_blank">
+                        Политика конфиденциальности
+                    </a>
                 </div>
 
-                <div className={cn(styles.contacts, styles.links)}>
-                    {socialMediaLinks.map(({ name, link, id }, i) => (
-                        <div
-                            key={id}
-                            className={cn(styles.contactsItem, {
-                                [styles.penultimate]: id === 4,
-                                [styles.last]:
-                                    i === socialMediaLinks.length - 1,
-                            })}
-                        >
-                            <a href={link} target="_blank">
-                                {name}
-                            </a>
-                        </div>
-                    ))}
+                <div className={styles.bottomContainerItem}>
+                    <a href={links.userAgreement} target="_blank">
+                        Пользовательское соглашение
+                    </a>
                 </div>
             </div>
 
-            <hr className={styles.line} />
-
-            <div className={cn(styles.contacts, styles.bottomContainer)}>
-                <div className={styles.bottomContainerFirstBlock}>
-                    <div
-                        className={cn(
-                            styles.bottomContainerItem,
-                            styles.bottomContainerFistItem
-                        )}
-                    >
-                        <a href={links.privacyPolicy} target="_blank">
-                            Политика конфиденциальности
-                        </a>
-                    </div>
-
-                    <div className={styles.bottomContainerItem}>
-                        <a href={links.userAgreement} target="_blank">
-                            Пользовательское соглашение
-                        </a>
-                    </div>
-                </div>
-
-                <div>
-                    <div className={styles.bottomContainerItem}>
-                        <a href={links.presentation} target="_blank">
-                            Открыть презентацию
-                        </a>
-                    </div>
+            <div>
+                <div className={styles.bottomContainerItem}>
+                    <a href={links.presentation} target="_blank">
+                        Открыть презентацию
+                    </a>
                 </div>
             </div>
         </div>
-    </footer>
+    </FooterContactsContainer>
 );
