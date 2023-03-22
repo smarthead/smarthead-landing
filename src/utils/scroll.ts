d59bf9c05e30d34d5a0b4e83f3b3f18a9a0360e6
Nikita Negodov <nn@smarthead.ru>
2023-03-22 13:02:06 +0300

    SHNSITE-40 returned scrollToSection function with changed interface

---
+++ src/utils/scroll.ts
@@ -0,0 +1,34 @@
+import { gsap } from 'gsap';
+import ScrollToPlugin from 'gsap/ScrollToPlugin';
+
+interface ScrollToSectionArgs {
+    section: string | null;
+    duration?: number;
+    onStart?: () => void;
+    onComplete?: () => void;
+}
+
+export const scrollToSection = ({
+    section,
+    duration,
+    onStart,
+    onComplete,
+}: ScrollToSectionArgs) => {
+    if (section === null || document.querySelector(section) === null) return;
+
+    gsap.registerPlugin(ScrollToPlugin);
+    gsap.to(window, {
+        duration: duration ? duration : 0.7,
+        scrollTo: {
+            y: section,
+        },
+        ease: 'power1.inOut',
+        overwrite: true,
+        onStart: onStart
+            ? onStart
+            : () => {
+                  window.history.pushState(null, '', section);
+              },
+        onComplete: onComplete,
+    });
+};
