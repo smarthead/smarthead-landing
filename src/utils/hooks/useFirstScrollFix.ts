48406b5056fb7bdb0d17f1cab2c37d0bbfca36de
Nikita Negodov <nn@smarthead.ru>
2023-03-22 15:04:54 +0300

    SHNSITE-39 added initial scroll-behavior: smooth;

--- src/utils/hooks/useFirstScrollFix.ts
+++ src/utils/hooks/useFirstScrollFix.ts
@@ -7,18 +7,7 @@ export function useFirstScrollFix() {
         if (hash.length > 0) {
             scrollToSection({
                 section: hash,
-                duration: 0,
-                onComplete: () => {
-                    window.document.body.style.scrollBehavior = 'smooth';
-                    document.getElementsByTagName(
-                        'html'
-                    )[0].style.scrollBehavior = 'smooth';
-                },
             });
-        } else {
-            window.document.body.style.scrollBehavior = 'smooth';
-            document.getElementsByTagName('html')[0].style.scrollBehavior =
-                'smooth';
         }
     }, []);
 }
